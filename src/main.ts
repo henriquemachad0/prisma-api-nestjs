e0394ab23486734f7823fc01e61d4af2d5ca13e1
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-08-22 11:10:38 -0300

    feat: add unauthorized interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
